834dcdc08fc110c52cc3507c730f57d851903318
Bagheri <4hm4db4gheri@gmail.com>
2023-11-22 14:36:58 +0330

    complete article page front

--- client/src/assets/scripts/Pages/Article.tsx
+++ client/src/assets/scripts/Pages/Article.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import "../../styles/index.scss";
 import "../../styles/reset.scss";
+import "../../styles/article.scss"
 
 export function Article() {
 	// Use the useLocation hook to access the location state
 	const location = useLocation();
-	const { Title, Date, TimeSpent, Content, ImageSrc } = location.state || {};
+	const { Title, Date, TimeSpent, Content } = location.state || {};
 	console.log(location.state);
-	
+
 	return (
 		<>
-			<header className="post-header">
+			<header className="article-header">
 				<div>
 					<div>
 						<Link to="/">
@@ -31,9 +31,9 @@ export function Article() {
 					</div>
 				</div>
 			</header>
-			<body>
-				<h1>{Title}</h1>
-				<div>
+			<body className="article-body">
+				<h1 className="article-title">{Title}</h1>
+				<div className="article-information">
 					<img
 						className="img-header"
 						src="assets/images/developer-high-resolution-logo.png"
@@ -42,14 +42,40 @@ export function Article() {
 						<h3>Ahmad Bagheri</h3>
 					</span>
 					<span>
-						<p>{Date}</p>
+						<p>  {Date}</p>
 					</span>
 					<span>
 						<i className="fa-solid fa-book-open"></i>
 					</span>
 					<span>
-						<p>{TimeSpent}</p>
+						<p>  {TimeSpent}</p>
 					</span>
+				</div>
+				<div className="article-content">
+					<h1>{Title}</h1>
+					<p>{Content}</p>
+				</div>
+				<div className="article-footer">
+					<h3>Written by</h3>
+					<div>
+						<div>
+							<span>
+								<img
+									className="img-header"
+									src="assets/images/developer-high-resolution-logo.png"
+								/>
+							</span>
+							<span>
+
+								<p className="writer">Ahmad Bagheri</p>
+								<p className="writer-info">Junior Developer, Gamer, Student at SBU</p>
+
+							</span>
+						</div>
+						<div>
+
+						</div>
+					</div>
 				</div>
 			</body>
 		</>
